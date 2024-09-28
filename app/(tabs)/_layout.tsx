f99a9fea536f2c8ac88b0f29d167de619b84aaa6
Pranjal Snehi <pranjal.snehi@gmail.com>
2024-09-28 17:48:28 +0530

    Animated icons using rive

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,10 +4,24 @@ import React from 'react';
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { View } from 'react-native';
+import Rive from 'rive-react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
-
+  function RiveDemo() {
+    return (
+      <View>
+        {/* Rive animation loaded from a URL */}
+        <Rive
+          url="https://public.rive.app/community/runtime-files/2195-4346-avatar-pack-use-case.riv"
+          artboardName="Avatar 1"
+          stateMachineName="avatar"
+          style={{ width: 36, height: 36 }}
+        />
+      </View>
+    );
+  }
   return (
     <Tabs
       screenOptions={{
@@ -18,8 +32,8 @@ export default function TabLayout() {
         name="index"
         options={{
           title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          tabBarIcon: () => (
+            <RiveDemo />
           ),
         }}
       />
